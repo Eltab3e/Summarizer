7ea6102341d12b18502031f44a2e74dcc366c608
Eltab3e <ahmed.eltabiie@gmail.com>
2023-11-09 21:46:52 +0200

    finalize & refactor

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,29 +6,26 @@ const Hero = () => {
             <nav className="flex justify-between items-center w-full mb-10 pt-3">
                 <img
                     src={logo}
-                    alt="summary-logo"
+                    alt="sumz_logo"
                     className="w-28 object-contain"
                 />
 
                 <button
                     type="button"
-                    onClick={() => {
-                        window.open("https://github.com/Eltab3e");
-                    }}
+                    onClick={() => window.open("https://github.com/Eltab3e/Summarizer", "_blank")}
                     className="black_btn"
                 >
-                    Github
+                    GitHub
                 </button>
             </nav>
 
-            <h1 className="head_text ">
-                Summarize Articles With <br className="max-md:hidden" />
-                <span className="orange_gradient">OpenAI GPT-4</span>
+            <h1 className="head_text">
+                Summarize Articles with <br className="max-md:hidden" />
+                <span className="orange_gradient ">OpenAI GPT-4</span>
             </h1>
-
             <h2 className="desc">
-                Simplify your reading with Summarize, an open-source article summarizer that
-                transforms lengthy articles into clear and concise summaries.
+                Simplify your reading with Summize, an open-source article summarizer that
+                transforms lengthy articles into clear and concise summaries
             </h2>
         </header>
     );
